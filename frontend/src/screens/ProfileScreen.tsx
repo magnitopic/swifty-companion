5bcce886fd0e8b286181ef5f988671c1fff5b6d2
magnitopic <magnitopic@gmail.com>
2026-01-29 18:09:10 +0100

    feat: getting user login titles

--- frontend/src/screens/ProfileScreen.tsx
+++ frontend/src/screens/ProfileScreen.tsx
@@ -6,15 +6,33 @@ import { getUser } from "../services/api";
 const ProfileScreen: React.FC = () => {
 	const [userId, setUserId] = useState<string>("NULL");
 	const [userName, setUserName] = useState<string>("NULL");
+	const [loginTitle, setLoginTitle] = useState<string>("NULL");
 
 	useEffect(() => {
 		// Use api service to fetch user profile data
 		const fetchUserProfile = async () => {
 			try {
-				const profile = await getUser("alaparic");
+				const profile = await getUser("fdiaz-gu");
+
+				let title_id = profile.titles_users.filter(
+					(title) => title.selected === true
+				)[0]?.title_id;
+
+				let fullUserLogin = title_id
+					? profile.titles.find((title) => title.id === title_id)
+							?.name
+					: null;
+
+				if (fullUserLogin != null) {
+					fullUserLogin = fullUserLogin.replace(
+						"%login",
+						profile.login
+					);
+				} else fullUserLogin = profile.login;
 
 				setUserId(profile.id);
 				setUserName(profile.login);
+				setLoginTitle(fullUserLogin);
 			} catch (error) {
 				console.error("Failed to fetch user profile:", error);
 			}
@@ -30,6 +48,7 @@ const ProfileScreen: React.FC = () => {
 			<Text className="text-white">Profile Screen</Text>
 			<Text className="text-font-main">{userId}</Text>
 			<Text className="text-font-main">{userName}</Text>
+			<Text className="text-font-main">{loginTitle}</Text>
 		</View>
 	);
 };
